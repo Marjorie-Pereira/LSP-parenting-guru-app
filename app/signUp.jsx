4139f42beaa911dac1f57fb42c8bafec1e8ee6fc
Marjorie-Pereira <marjoriepereira@rede.ulbra.br>
2025-08-31 22:43:03 -0300

    tela cadastro

---
+++ app/signUp.jsx
@@ -0,0 +1,135 @@
+import { useRouter } from 'expo-router';
+import { useRef, useState } from 'react';
+import { Alert, Pressable, StatusBar, StyleSheet, Text, View } from 'react-native';
+import Mail from '../assets/icons/Mail';
+import User from '../assets/icons/User';
+import ViewIcon from '../assets/icons/ViewIcon';
+import ViewIconSlash from '../assets/icons/ViewIconSlash';
+import BackButton from '../components/BackButton';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import ScreenWrapper from '../components/ScreenWrapper';
+import { theme } from '../constants/theme';
+import { hp, wp } from '../helpers/common';
+import { supabase } from '../lib/supabase';
+
+const SignUp = () => {
+  const router = useRouter();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const nameRef = useRef("");
+  const [loading, setLoading] = useState(false)
+  const [eyeIcon, setEyeIcon] = useState("hide")
+
+  const onSubmit = async () => {
+    if(!emailRef.current || !passwordRef.current) {
+      Alert.alert("Please fill all the fields")
+      return;
+    }
+
+    let name = nameRef.current.trim()
+    let email = emailRef.current.trim()
+    let password = passwordRef.current.trim()
+
+    setLoading(true)
+
+    const { data: {session}, error} = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    setLoading(false)
+    console.log("session: ", session);
+    console.log("error: ", error);
+    if(error) {
+      Alert.alert('Sign Up', error.message)
+      
+    }
+  }
+  return (
+    <ScreenWrapper bg='#E3E2EB'>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+
+        {/* title */}
+        <View>
+          <Text style={styles.welcomeText}>Create an account</Text>
+        </View>
+
+        {/* form */}
+        <View style={styles.form}>
+          <Text style={{fontSize: hp(2), color: theme.colors.text}}>
+            Please sign up to continue
+          </Text>
+
+          <Input 
+            placeholder="Enter your name"
+            icon={<User />}
+            onChangeText={(value) => nameRef.current = value}
+          />
+          <Input 
+            placeholder="Email address"
+            icon={<Mail />}
+            onChangeText={(value) => emailRef.current = value}
+          />
+          <Input 
+            placeholder="Password"
+            icon={eyeIcon === "hide" ? <ViewIcon /> : <ViewIconSlash />}
+            onChangeText={(value) => passwordRef.current = value}
+            togglePassword={() => {
+              setEyeIcon(eyeIcon === "hide" ? "show" : "hide")
+            }}
+            secureTextEntry={eyeIcon === "hide" ? true : false}
+            type="password"
+          />
+          
+          <Button title='Sign Up' loading={loading} onPress={onSubmit} />
+          
+        </View>
+        {/* footer */}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already have an account? </Text>
+          <Pressable onPress={() => router.push("./login")}>
+            <Text style={[styles.footerText, {color: theme.colors.primary, fontWeight: theme.fonts.semibold}]}>Log in</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ScreenWrapper>
+  )
+}
+
+export default SignUp
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 45,
+        paddingHorizontal: wp(5)
+    },
+    welcomeText: {
+      fontSize: hp(4),
+      fontWeight: theme.fonts.bold,
+      color: theme.colors.text
+    },
+    form: {
+      gap: 20
+    },
+    forgotPassword: {
+      textAlign: 'right',
+      fontWeight: theme.fonts.semibold,
+      color: theme.colors.text,
+      fontSize: hp(2)
+    },
+    footer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 5
+    },
+    footerText: {
+      textAlign: 'center',
+      color: theme.colors.text,
+      fontSize: hp(1.6)
+    }
+
+})
