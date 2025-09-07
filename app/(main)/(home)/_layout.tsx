5648869fe704e6dde070dc5c76ac5ce02f57f830
Marjorie-Pereira <marjoriepereira@rede.ulbra.br>
2025-09-07 16:25:52 -0300

    icones nas tabs

--- app/(main)/(home)/_layout.tsx
+++ app/(main)/(home)/_layout.tsx
@@ -1,26 +1,48 @@
+import { theme } from '@/constants/theme';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 import React from 'react';
-// import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{headerShown:false}}>
-      <Tabs.Screen
-        name="(tabs)/schedules"
-        options={{
-            tabBarLabel: "Agendas"
-        }}
-      />
+    
+    <Tabs screenOptions={{headerShown:false, tabBarActiveTintColor: theme.colors.primary}}>
       <Tabs.Screen
         name="home"
         options={{
-            tabBarLabel: "Início"
+            tabBarLabel: "Início",
+            tabBarIcon: ({size, color}) => (
+                <Ionicons name="home-outline" size={size} color={color} />
+            )
         }}
       />
+
+      <Tabs.Screen
+        name="schedules"
+        options={{
+            tabBarLabel: "Agendas",
+            tabBarIcon: ({size, color}) => (
+                <Ionicons name="calendar-clear-outline" size={size} color={color} />
+            )
+        }}
+      />
+      
       <Tabs.Screen
         name="trackerDevice"
         options={{
-            tabBarLabel: "Viagens"
+            tabBarLabel: "Viagens",
+            tabBarIcon: ({size, color}) => (
+                <Ionicons name="map-outline" size={size} color={color} />
+            )
+        }}
+      />
+      <Tabs.Screen
+        name="mindfulness"
+        options={{
+            tabBarLabel: "Autocuidado",
+            tabBarIcon: ({size, color}) => (
+                <Ionicons name="medical-outline" size={size} color={color} />
+            )
         }}
       />
 
