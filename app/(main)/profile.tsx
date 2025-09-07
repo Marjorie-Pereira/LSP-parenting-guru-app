98862381c97af005917b36e6ae960a9b7634087f
Marjorie-Pereira <marjoriepereira@rede.ulbra.br>
2025-09-07 16:26:14 -0300

    logout transferido para tela profile

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import Button from '@/components/Button';
+import ScreenWrapper from '@/components/ScreenWrapper';
+import { supabase } from '@/lib/supabase';
+import React from 'react';
+import { Alert, Text } from 'react-native';
 
-export default function profile() {
-  return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
+export default function Profile() {
+  const onLogout = async () => {     
+    const { error } = await supabase.auth.signOut();
+    if(error) {
+        Alert.alert('Logout', error.message)
+    }
+  }
+  
+    return (
+      <ScreenWrapper>
+        
+        <Text>Profile Info</Text>
+        <Button title='Sair' onPress={onLogout} />
+      </ScreenWrapper>
+      
+    )
 }
